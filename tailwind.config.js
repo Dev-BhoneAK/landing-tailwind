7d9966868f56f71e4fe510259abdec5e9d62a5d9
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-07-14 23:27:47 +0630

    feat: add container padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/*.html"],
   theme: {
-    // screens: {
-    //   xs: "300px",
-    //   sm: "480px",
-    //   md: "768px",
-    //   lg: "976px",
-    //   xl: "1440px",
-    // },
+    screens: {
+      // => 0 ===> 640px All phone size including
+
+      sm: "640px",
+      // 640px ===> 768px Ipad Mini, Bigger Phone, etc
+
+      md: "768px",
+      // 768px ===> 1024px Ipad
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // 1280px ===> 1536px Macbook air, Laptops
+
+      "2xl": "1536px",
+      // desktop monitors
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
         primaryColor: "#242d52",
